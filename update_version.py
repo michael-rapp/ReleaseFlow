8bac854a1f9dde8bb1e7c7402d5d8ed84ff58b87
Michael Rapp <michael.rapp90@googlemail.com>
2024-11-01 13:41:27 +0100

    Add dataclass Version.

--- update_version.py
+++ update_version.py
@@ -1,12 +1,29 @@
-from typing import Tuple, Optional
+from dataclasses import dataclass
+from typing import Optional
 
 VERSION_FILE = 'VERSION'
+
+
+@dataclass
+class Version:
+    major: int
+    minor: int
+    patch: int
+    dev: Optional[int] = None
+
+    def __str__(self) -> str:
+        version = str(self.major) + '.' + str(self.minor) + '.' + str(self.patch)
+
+        if self.dev:
+            version += '.dev' + str(self.dev)
+
+        return version
 
 
 def __read_version_file() -> str:
     with open(VERSION_FILE, mode='r') as version_file:
         lines = version_file.readlines()
-        
+
         if len(lines) != 1:
             print('File "' + VERSION_FILE + '" must contain exactly one line')
             exit(-1)
@@ -14,7 +31,7 @@ def __read_version_file() -> str:
         return lines[0]
 
 
-def __parse_version(version: str) -> Tuple[str]:
+def __parse_version(version: str) -> Version:
     error_message = 'Version must be given in format MAJOR.MINOR.PATCH or MAJOR.MINOR.PATCH.devN, but got: ' + version
     parts = version.split('.')
 
@@ -25,25 +42,16 @@ def __parse_version(version: str) -> Tuple[str]:
     if len(parts) > 3 and not parts[3].startswith('dev'):
         print(error_message)
         exit(-1)
-    
+
     try:
         major = int(parts[0])
         minor = int(parts[1])
         patch = int(parts[2])
         dev = int(parts[3][len('dev'):]) if len(parts) > 3 else None
-        return major, minor, patch, dev
+        return Version(major=major, minor=minor, patch=patch, dev=dev)
     except ValueError:
         print(error_message)
         exit(-1)
-
-
-def __format_version(major: int, minor: int, patch: int, dev: Optional[int]) -> str:
-    version = str(major) + '.' + str(minor) + '.' + str(patch)
-
-    if dev:
-        version += '.dev' + str(dev)
-    
-    return version
 
 
 def __write_version_file(version: str):
@@ -51,49 +59,49 @@ def __write_version_file(version: str):
         version_file.write(version)
 
 
-def __get_current_version() -> Tuple[int]:
+def __get_current_version() -> Version:
     current_version = __read_version_file()
     print('Current version is "' + current_version + '"')
     return __parse_version(current_version)
 
 
-def __update_version(major: int, minor: int, patch: int, dev: Optional[int]):
-    updated_version = __format_version(major, minor, patch, dev)
+def __update_version(version: Version):
+    updated_version = str(version)
     print('Updated version to "' + updated_version + '"')
     __write_version_file(updated_version)
 
 
 def drop_development_version():
-    major, minor, patch, dev = __get_current_version()
-    dev = None
-    __update_version(major, minor, patch, dev)
+    version = __get_current_version()
+    version.dev = None
+    __update_version(version)
 
 
 def increment_development_version():
-    major, minor, patch, dev = __get_current_version()
-    dev = 1 if dev is None else dev + 1
-    __update_version(major, minor, patch, dev)
+    version = __get_current_version()
+    version.dev = 1 if version.dev is None else version.dev + 1
+    __update_version(version)
 
 
 def increment_patch_version():
-    major, minor, patch, dev = __get_current_version()
-    patch += 1
-    dev = None
-    __update_version(major, minor, patch, dev)
+    version = __get_current_version()
+    version.patch += 1
+    version.dev = None
+    __update_version(version)
 
 
 def increment_minor_version():
-    major, minor, patch, dev = __get_current_version()
-    minor += 1
-    patch = 0
-    dev = None
-    __update_version(major, minor, patch, dev)
+    version = __get_current_version()
+    version.minor += 1
+    version.patch = 0
+    version.dev = None
+    __update_version(version)
 
 
 def increment_major_version():
-    major, minor, patch, dev = __get_current_version()
-    major += 1
-    minor = 0
-    patch = 0
-    dev = None
-    __update_version(major, minor, patch, dev)
+    version = __get_current_version()
+    version.major += 1
+    version.minor = 0
+    version.patch = 0
+    version.dev = None
+    __update_version(version)
